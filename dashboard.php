7e38fd53df86fda94ed7cd8a6cd06f30ffb85db8
sandhiyaa151-dot <sandhiyaa151@gmail.com>
2026-05-04 19:44:03 +0530

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Dashboard</title>
+</head>
+<body>
+
+<h2>Welcome to Student Management System</h2>
+
+<a href="add_student.php">Add Student</a><br>
+<a href="view_student.php">View Students</a><br>
+<a href="edit_student.php">Edit Student</a><br>
+<a href="delete_student.php">Delete Student</a><br>
+<a href="logout.php">Logout</a>
+
+</body>
+</html>
---
+++ design.php
@@ -0,0 +1,18 @@
+<style>
+body {
+    font-family: Arial;
+    text-align: center;
+}
+
+a {
+    display: block;
+    margin: 10px;
+    padding: 10px;
+    background: blue;
+    color: white;
+    text-decoration: none;
+    width: 200px;
+    margin-left: auto;
+    margin-right: auto;
+}
+</style>
